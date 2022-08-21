06d7710f1e334483608ce8323d804cf63c9a6848
Spsden <pratapsinghsuraj420@gmail.com>
2022-08-22 00:41:31 +0530

    sidebar highlight

--- Components/SideBar/Sidebar.js
+++ Components/SideBar/Sidebar.js
@@ -1,31 +1,73 @@
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 import styles from "./Sidebar.module.css";
 import { SidebarData } from "./SidebarData";
 
 function Sidebar() {
+  const [current, setCurrent] = useState("/home");
+
+  const router = useRouter();
+
+  if (router.asPath === current) {
+  }
+
   return (
     <div className={styles.sidebar}>
       <div className={styles.rushname}>Rush</div>
 
       <nav>
+
+      {SidebarData.map((val, key) => {
+            return (
+
+              <div key={key} className = {styles.siderow}>
+                <Link key={key} href={val.link} >
+                  <a
+                    className={
+                      router.asPath === val.link
+                        ? styles.active
+                        : styles.rowblock
+                    }
+                  >
+
+                    
+                    <div id={styles.icon}>{val.icon}</div>
+                    <div id={styles.title}>{val.title}</div>
+                  </a>
+                </Link>
+
+              </div>
+              
+                
+              
+            );
+          })}
+{/* 
+        
         <ul className={styles.sidebarlist}>
           {SidebarData.map((val, key) => {
             return (
-              <li key={key} >
-                <Link href={val.link}>
-                  <a className= {styles.rowblock}>
+              <li key={key}>
+                <Link  href={val.link}>
+                  <a
+                    className={
+                      router.asPath === val.link
+                        ? styles.active
+                        : styles.rowblock
+                    }
+                  >
+
                     
-                      <div id={styles.icon}>{val.icon}</div>
-                      <div id={styles.title}>{val.title}</div>
-                    
+                    <div id={styles.icon}>{val.icon}</div>
+                    <div id={styles.title}>{val.title}</div>
                   </a>
                 </Link>
               </li>
             );
           })}
-        </ul>
+        </ul> */}
       </nav>
 
       {/* <ul className={styles.sidebarlist}>
