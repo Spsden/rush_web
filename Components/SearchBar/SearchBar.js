8c18a827001309219b22b196250edab4365ef468
Spsden <pratapsinghsuraj420@gmail.com>
2022-08-25 21:48:00 +0530

    Search suggestions in progress

--- Components/SearchBar/SearchBar.js
+++ Components/SearchBar/SearchBar.js
@@ -1,18 +1,40 @@
 import styles from "./SearchBar.module.css";
 import { Layout, Search } from "react-feather";
 import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import useDebounce from "../Hooks/useDebounce";
+import React, { useState } from "react";
 
 function SearchBar() {
   const router = useRouter();
-  const [query, setQuery] = useState();
+  const [query, setQuery] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  const modalHandler = () => {
+    setVisible(!visible);
+  };
+
+  const Suggestions = PopularApps.filter(item => {
+    const searchQuery = query.toLowerCase();
+    const entireList = item.toLowerCase();
+   
+
+    return searchQuery && entireList.startsWith(searchQuery);
+  })
+
+  // useEffect(() => {
+
+  //   const closeModal = e => {
+  //     if(e.path[0] !== modalRef.current)
+  //     setVisible(false);
+  //   }
+  //   document.body.addEventListener('click',closeModal);
+
+  //   return () => document.removeEventListener('click',closeModal);
+  // },[])
 
   const handleEnterKey = (e) => {
     e.preventDefault();
 
-     if (e.key === "Enter") {
+    if (e.key === "Enter") {
       console.log("pressed");
       const q = e.currentTarget.value;
       router.push(
@@ -28,69 +50,53 @@ function SearchBar() {
     }
   };
 
-  // const handlebutton =(some)=> {
-
-  //   if(some != ""){
-
-  //     console.log("");
-  //    // const q = e.currentTarget.value;
-  //     router.push(
-  //       {
-  //         pathname: `/searchresults`,
-  //         query: some ? { some } : {},
-  //       },
-  //       undefined,
-  //       {
-  //         shallow: false,
-  //       }
-  //     );
-
-
-  //   } else{
-  //     console.log("emptuy")
-  //   }
-
-   
-  // }
-
   return (
     <div className={styles.card}>
-      {/* <form onSubmit={handleEnterKey} className={styles.form}> */}
-      <input
-        type="search"
-        className={styles.input}
-        //value={searchQuery}
-        placeholder="Search app"
-        onKeyUp={handleEnterKey}
-        onChange={(e) => {
-          setQuery(e.target.value);
-        }}
-        // onFocus={() => {
-        //   router.push('/searchresults'
+      <div className={styles.bar}>
+        {/* <form onSubmit={handleEnterKey} className={styles.form}> */}
+        <input
+          // ref={modalRef}
+          type="search"
+          className={styles.input}
+          //value={searchQuery}
+          placeholder="Search app"
+          onKeyUp={handleEnterKey}
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          onFocus={() => {
+            console.log("beep");
+            setVisible(!visible);
 
-        //   //   {
-        //   //   pathname: '/searchresults',
-        //   //   query : {
-        //   //     result : 'dripp'
-        //   //   }
-        //   // }
+            // modalHandler()
+          }}
+        ></input>
+        {/* </form> */}
 
-        //   );
-        // }}
-      ></input>
-      {/* </form> */}
+        <button
+          className={styles.button}
+          onClick={(e) => {
+            console.log(e.target.value);
+          }}
+        >
+          <Search />
+        </button>
+      </div>
+      {Suggestions.length == 0 ? <></> :<div className={styles.suggestionsbar}>
+      {Suggestions.map((value, key) => {
+        return (
+          <div key={key} className={styles.suggestionitem} >
+            <p>{value}</p>
+          </div>
+        );
+      })}
 
-      <button
-        className={styles.button}
-        onClick={(e)=>{
-          console.log(e.target.value)
-        }}
-        
-      >
-        <Search />
-      </button>
+      </div>}
+      
+      
     </div>
   );
 }
+import { PopularApps } from "./SearchSuggestionsData";
 
 export default SearchBar;
