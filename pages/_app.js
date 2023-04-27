4cb56b92e837e1827d2e0dc3ef21f49ebd29b3ba
Spsden <pratapsinghsuraj420@gmail.com>
2023-04-27 19:08:56 +0530

    migration to next 13 begins

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,22 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import { Layout } from '../Components/Layout/Layout';
-import '../styles/globals.css'
-import App from 'next/app';
-import { useRouter } from 'next/router';
-import SearchPage from './searchresults';
-import { DisplaySection } from '../Components/DisplaySection/DisplaySection';
-
-
-
-
-
-
-
-
-
-
+import { Layout } from "../Components/Layout/Layout";
+import "../styles/globals.css";
+import App from "next/app";
+import { useRouter } from "next/router";
+import SearchPage from "./searchresults";
+import { DisplaySection } from "../Components/DisplaySection/DisplaySection";
 
 function MyApp({ Component, pageProps }) {
+  const getLayout = Component.Layout || Layout;
 
-
-
-  const getLayout = Component.Layout || Layout
-  
-  
   return (
-  
-  <Layout>
-    
-    <Component {...pageProps}/>
-    
-    </Layout>);
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
-
+export default MyApp;
 
 //DisplaySection is nested inside Layout and Layout is nested in MyApp
